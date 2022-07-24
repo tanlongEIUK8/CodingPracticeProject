e7db011a2905d6ff41cfa6d61746c7f739717bac
longtanK8 <tan.long.9699@gmail.com>
2022-07-24 15:05:28 +0700

    initial form

--- src/ui/Register.java
+++ src/ui/Register.java
@@ -1,5 +1,29 @@
 package ui;
 
-public class Register {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Register extends JFrame{
+    private JButton Test;
+    private JPanel Main;
     //hello
+
+    public Register() {
+        Test.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        Register r = new Register();
+        r.setContentPane(new Register().Main);
+        r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        r.setVisible(true);
+        r.pack();
+    }
 }
+
