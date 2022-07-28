f6fc283e8f1e1b7b25194158489453bd898275fe
Phu Vo <phu_vo@datahouse.com>
2022-07-28 15:39:01 +0700

    update UI

---
+++ src/ui/Login.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javax.swing.*;
+
+public class Login {
+
+    private JTextField txt_Register;
+    private JPanel loginForm;
+    private JTextField txt_username;
+    private JTextField input_username;
+    private JPanel background;
+    private JTextField txt_password;
+    private JPasswordField input_password;
+    private JButton btn_login;
+    private JTextField txt_forgotpassword;
+    private JTextField DONTHAVEANACCOUNTTextField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/ui/MainChat.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javax.swing.*;
+
+public class MainChat {
+    private JLabel png_avatar;
+    private JTextField txt_contact;
+    private JTextField txt_username;
+    private JTextField txt_groupcontact;
+    private JTextField txt_groupname;
+    private JLabel png_groupavatar;
+    private JTextField txt_username1;
+    private JLabel png_useravatar1;
+    private JTextField input_mess;
+    private JLabel png_fileicon;
+    private JLabel png_picicon;
+    private JLabel png_sendicon;
+}
